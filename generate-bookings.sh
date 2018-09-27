327fc3a160cc9da9c322c6ec6268f670ee65f464
Juergen Etzlstorfer <7677092+jetzlstorfer@users.noreply.github.com>
2018-09-27 10:57:34 +0200

    improved load generation by deleting tickets after a while

--- generate-bookings.sh
+++ generate-bookings.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 echo "Press [CTRL+C] to stop.."
+i=0
 while true
 do
   echo ""
   echo "ordering a ticket..."
   curl -X POST -H "Content-Type: application/json" -d "{\"ticketRequests\":[{\"ticketPrice\":1,\"quantity\":1}],\"email\":\"foo@bar.coom\",\"performance\":1}" http://backend-v2-ticketmonster.18.194.120.69.xip.io/rest/bookings/
-	sleep 1
+  i=$((i+1))
+  # delete all tickets if more than 100 already booked by this script
+  if [ $i -gt 100 ]  
+  then 
+    echo "delete all tickets ordered so far..."
+    curl -X PUT -H "Content-Type: application/json" -d "\"RESET\"" http://monolith-ticketmonster.18.194.120.69.xip.io/rest/bot/status
+    let i=0
+  fi
+  sleep 1
 done
