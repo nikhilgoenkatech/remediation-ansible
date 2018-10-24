113d6ebae06323dd341717244358acb45c6dc1df
Juergen Etzlstorfer <7677092+jetzlstorfer@users.noreply.github.com>
2018-10-24 15:41:04 +0200

    moving urls to variables

--- generate-bookings.sh
+++ generate-bookings.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "Press [CTRL+C] to stop.."
 
+bookingUrl=http://backend-v2-ticketmonster.18.194.120.69.xip.io/rest/bookings/
+statusUrl=http://backend-v2-ticketmonster.18.194.120.69.xip.io/rest/bot/status
+
 # delete all bookings so far
- curl -X PUT -H "Content-Type: application/json" -d "\"RESET\"" http://backend-v2-ticketmonster.18.194.120.69.xip.io/rest/bot/status
+ curl -X PUT -H "Content-Type: application/json" -d "\"RESET\"" $statusUrl
 sleep 1
 
 i=0
@@ -10,13 +13,14 @@ while true
 do
   echo ""
   echo "ordering a ticket..."
-  curl -X POST -H "Content-Type: application/json" -d "{\"ticketRequests\":[{\"ticketPrice\":1,\"quantity\":1}],\"email\":\"foo@bar.coom\",\"performance\":1}" http://backend-v2-ticketmonster.18.194.120.69.xip.io/rest/bookings/
+  curl -X POST -H "Content-Type: application/json" -d "{\"ticketRequests\":[{\"ticketPrice\":1,\"quantity\":1}],\"email\":\"foo@bar.coom\",\"performance\":1}" $bookingUrl
   i=$((i+1))
   # delete all tickets if more than 100 already booked by this script
   if [ $i -gt 100 ]  
   then 
+    echo ""
     echo "delete all tickets ordered so far..."
-    curl -X PUT -H "Content-Type: application/json" -d "\"RESET\"" http://backend-v2-ticketmonster.18.194.120.69.xip.io/rest/bot/status
+    curl -X PUT -H "Content-Type: application/json" -d "\"RESET\"" $statusUrl
     let i=0
   fi
   sleep 1
